eb171fede02b043a53cebb00e30222e34315f3b5
janevnikola <72938546+janevnikola@users.noreply.github.com>
2022-12-22 13:37:40 +0100

    Create primer_tabela.sql

---
+++ primer_tabela.sql
@@ -0,0 +1,15 @@
+CREATE TABLE DEPT (
+  DNAME VARCHAR(10) NOT NULL,
+  DNUMBER INTEGER NOT NULL,
+  MGRSSN CHAR(9),
+  MGRSTARTDATE CHAR(9),
+  /*primaren kluc e dnumber*/
+  PRIMARY KEY (DNUMBER),
+  /*DNAME e edinstven atribut*/
+  UNIQUE (DNAME),
+  /*Nadvoresen kluc vo nasata tabela MGRSSN se koristi od tabelata EMP(SSN)*/
+  FOREIGN KEY (MGRSSN) REFERENCES EMP(SSN)
+  /*vidi so znacat ovie*/
+  on DELETE SET DEFAULT
+  ON UPDATE CASCADE
+);
