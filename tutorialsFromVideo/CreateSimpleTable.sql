de2d81cb355e731e7162004b3917d0faf190dd6d
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-28 20:41:40 +0100

    Create CreateSimpleTable.sql

---
+++ tutorialsFromVideo/CreateSimpleTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student(
+	student_id INT, --ova ke bide primaren kluc so tip integer
+  	name VARCHAR(20), --ime na studentot so data type varchar so length 20
+  	nasoka VARCHAR(20),
+  	PRIMARY KEY(student_id) --vikame deka primaren kluc ke bide student id
+  	
+);
