362ae946e057495400418236f231366f95f7faa5
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-28 20:47:37 +0100

    Update DodavanjeNovaKolona.sql

--- tutorialsFromVideo/DodavanjeNovaKolona.sql
+++ tutorialsFromVideo/DodavanjeNovaKolona.sql
@@ -1,1 +1,2 @@
-ALTER TABLE student ADD gpa DECIMAL; --dodavanje na nova kolona, vo ovoj slucaj dodavame gpa od tip decimal
+ALTER TABLE student ADD gpa DECIMAL(3,2); --dodavanje na nova kolona, vo ovoj slucaj dodavame gpa od tip decimal
+--(3 broevi, a 2 broevi se posle tockata, primer 3.22) 
