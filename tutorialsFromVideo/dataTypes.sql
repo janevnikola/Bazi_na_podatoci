380b5a6ecd3af647f33a2c7301e4436a6dca8af9
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-28 20:20:34 +0100

    Create dataTypes.sql

---
+++ tutorialsFromVideo/dataTypes.sql
@@ -0,0 +1,7 @@
+/*data types*/
+INT --celi broevi
+DECIMAL(M,N) --decimalni broevi
+VARCHAR(length) --string of text so length
+BLOB --Binary large object, stores large date, pr: image, file, videos itn
+DATE --date: yyyy-mm-dd
+TIMESTAMP --timestamp yyyy-mm-dd hh:mm:ss
