799e4fadf307c469f735f5b0d3cbf09ecffa6398
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-28 20:54:11 +0100

    Create InsertIntoTables.sql

---
+++ tutorialsFromVideo/InsertIntoTables.sql
@@ -0,0 +1,2 @@
+INSERT INTO student(student_id,name,nasoka,gpa) VALUES(1,"Nikola","IMB",9.72); --vnesuvame vo tabelata student, vo kolonite student_id,name,nasoka,gpa
+--so vrednosti VALUES(1,"Nikola","IMB",9.72)
