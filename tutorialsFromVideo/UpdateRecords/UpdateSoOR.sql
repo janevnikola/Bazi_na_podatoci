936166a855cc0ad0813ec2135abfb087a7623b3d
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-29 12:20:04 +0100

    Create UpdateSoOR.sql

---
+++ tutorialsFromVideo/UpdateRecords/UpdateSoOR.sql
@@ -0,0 +1,3 @@
+UPDATE student --update na tabelata student
+SET nasoka="Eko-informatika" --setiraj nasoka="Eko informatika"
+WHERE nasoka="informatika" OR nasoka="Ekonomija"; --kade sto nasokata e informatika ili Ekonomija
