a5ce01782ad2781edd893cfd6468544d971bfa0e
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-01-29 12:16:18 +0100

    Delete Update_na_kolona.sql

--- tutorialsFromVideo/Update_na_kolona.sql
+++
@@ -1,4 +0,0 @@
---sakame nasokata vo tabelata student da se vika sega internet, mrezi i bezbednost namesto imb
-UPDATE student --sakame da ja update tabelata student
-SEt nasoka="Internet, mrezi i bezbednost" --setiraj ja nasokata vo internet, mrezi i bezbednost
-WHERE nasoka="IMB"; --sekade kade sto ke najdis nasoka IMB
