9c9b3d97ef269d3ca3893d48f42e15b48e506a86
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-02-04 14:22:27 +0100

    Create KoristenjeNaAs.sql

---
+++ tutorialsFromVideo/proekt/SELECT zadaci/KoristenjeNaAs.sql
@@ -0,0 +1,3 @@
+--najdi gi site imiminja i preziminja ama kolonite da se ime za first_name i prezime za last_name na site employees
+SELECT first_name AS ime, last_name AS prezime --so AS davame alijansa na kolonata, no imeto na kolonata nema da se smeni originalno tuku samo za ovoj select
+FROM employee;
