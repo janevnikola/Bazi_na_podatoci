f0939e9e86d58d0717645b953c3824c7b65dcff7
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-02-04 12:23:57 +0100

    Create employee.sql

---
+++ tutorialsFromVideo/proekt/employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employee(
+	employee_id INTEGER,
+  	first_name VARCHAR(40),
+  	last_name VARCHAR(40),
+  	birthday DATE,
+  	sex VARCHAR(1),
+  	salary INTEGER,
+  	supervisor_id INTEGER,
+  	branch_id INTEGER,
+  	PRIMARY KEY (employee_id)
+);
