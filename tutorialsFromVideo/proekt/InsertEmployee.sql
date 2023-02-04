8f90ac4652c375fb99a74194f8fa7f645af3e702
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-02-04 13:47:31 +0100

    Create InsertEmployee.sql

---
+++ tutorialsFromVideo/proekt/InsertEmployee.sql
@@ -0,0 +1,2 @@
+INSERT INTO employee(employee_id,first_name,last_name,birthday,sex,salar,supervisor_id,branch_id) VALUES(100,"David","Wallace","1967-11-17","M",250000,NULL,NULL);
+--za supervisor_id i branch_id setirame null bidejki vo branch tabelata nemame kreirano branch,
