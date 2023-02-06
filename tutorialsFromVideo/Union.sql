b9fb11b0ffc6a0653f99d9ffe3ddee36cd6f6611
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-02-06 11:30:10 +0100

    Update Union.sql

--- tutorialsFromVideo/Union.sql
+++ tutorialsFromVideo/Union.sql
@@ -6,4 +6,13 @@ UNION
 SELECT branch_name
 FROM branch;
 
---ova ke gi dade iminjata i imeto na branch vo eden selekt
+--ova ke gi dade iminjata i imeto na branch vo eden selekt 
+--no mora da imame edna kolona so edna kolona
+
+--PRIMER:
+--ako imame vaka, ova ke dade error bidejki vo prviot select imame dve koloni(first name i last name) a vo vtoriot select samo eden (branch name)
+--SELECT first_name, last_name
+--FROM employee
+--UNION
+--SELECT branch_name
+--FROM branch;
